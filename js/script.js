03299cba6b5deefa751b1e933a949a3696c4f9d8
corazon469 <ramirez.samuelmeneses469@gmail.com>
2026-04-22 15:10:30 -0500

    asdfgh

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const secciones = [
+    {
+        nombre: "Diseño",
+        video: "videos/diseño.mp4"
+    },
+
+    {
+        nombre: "Ilustración",
+        video: "videos/ilustracion.mp4"
+    },
+    {
+        nombre: "Programación",
+        video: "videos/programacion.mp4"
+    }
+];
+
+let indiceActual = 0;
+let bloqueado = false;
+
+const video = document.getElementById("videoPlayer");
+const source = document.getElementById("videoSource");
+const texto = document.getElementById("seccionActual");
+
+function cambiarSeccion(direccion) {
+    if (bloqueado) return;
+    bloqueado = true;
+
+    indiceActual += direccion;
+
+    if (indiceActual < 0) indiceActual = 0;
+    if (indiceActual >= secciones.length) indiceActual = secciones.length - 1;
+
+    const nueva = secciones[indiceActual];
+
+    // cambiar texto
+    texto.textContent = ">" + nueva.nombre;
+
+    // cambiar video
+    source.src = nueva.video;
+    video.load();
+    video.play();
+
+    setTimeout(() => {
+        bloqueado = false;
+    }, 800); // evita que cambie muy rápido
+}
+
+// detectar scroll
+window.addEventListener("wheel", (e) => {
+    if (e.deltaY > 0) {
+        cambiarSeccion(1); // abajo
+    } else {
+        cambiarSeccion(-1); // arriba
+    }
+});
